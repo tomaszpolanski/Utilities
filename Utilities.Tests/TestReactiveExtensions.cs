b8e4b1f20d90bd2ad4ffa8c559e2c564840a8663
Tomasz Polanski <tomasz.polanski@futurice.com>
2014-10-10 14:31:48 +0200

    Adding unit test project

---
+++ Utilities.Tests/TestReactiveExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utilities.Reactive;
+
+namespace Utilities.Tests
+{
+    [TestClass]
+    public class TestReactiveExtensions
+    {
+        [TestMethod]
+        public void TestWhereIsNotNull()
+        {
+            var list = new List<string> {"a", null, "c"};
+
+            Assert.AreEqual(2, list.ToObservable().WhereIsNotNull().ToEnumerable().Count());
+        }
+    }
+}
