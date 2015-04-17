eedab3c6977ddbda8a840c4d42265bf413403538
Tomasz Polanski <krigerek@gmail.com>
2015-04-17 09:28:26 +0200

    Added converting void to unit

--- Utilities.Functional/Unit.cs
+++ Utilities.Functional/Unit.cs
@@ -1,8 +1,16 @@
-﻿namespace Utilities.Functional
+﻿using System;
+
+namespace Utilities.Functional
 {
     public class Unit
     {
         public static readonly Unit Default = new Unit();
+
+        public static Unit AsUnit(Action action)
+        {
+            action.Invoke();
+            return Default;
+        }
 
         private Unit() { }
     }
